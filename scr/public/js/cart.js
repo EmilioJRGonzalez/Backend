04f7cc05efc77ec6bc70cb10cb755352df2fc57e
EmilioJRGonzalez <emiliojrg88@gmail.com>
2024-07-02 00:26:39 -0300

    - Boton eliminar en carrito.

--- scr/public/js/cart.js
+++ scr/public/js/cart.js
@@ -13,3 +13,24 @@ async function purchase(cartId) {
         alert('Hubo un error al intentar realizar la compra.');
     }
 }
+
+async function deleteProduct(cartId, productId) {
+    // Lógica para eliminar el producto
+    console.log('Carrito:', cartId, 'Producto a eliminar:', productId);
+    
+    try {
+        const response = await fetch(`/api/cart/${cartId}/product/${productId}`, {
+            method: 'DELETE',
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error('Error al realizar la compra');
+        }
+        alert(data.message)
+        window.location.href = `/cart/${cartId}`;
+    } catch (error) {
+        console.error(error);
+        alert('Hubo un error al intentar realizar la compra.');
+    }
+}
